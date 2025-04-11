5c1387f63b9da4767ec4f06a0cdc792d948a5f3e
Eduardo Martinez <eduardo.z.martinez.mtz@gmail.com>
2025-04-10 21:30:03 -0500

    fixed app.tsx

--- App.tsx
+++ App.tsx
@@ -2,43 +2,11 @@ import "@expo/metro-runtime"
 import React from "react"
 import * as SplashScreen from "expo-splash-screen"
 import App from "./app/app"
-import { StyleSheet, TextStyle, Text as RNText, TextProps as RNTextProps } from "react-native"
-
-// 🧩 Patch 1: Log any invalid fontSize from StyleSheet.create
-const originalCreate = StyleSheet.create
-StyleSheet.create = (styles) => {
-  for (const key in styles) {
-    const style = styles[key]
-    const fontSize = (style as TextStyle)?.fontSize
-    if (typeof fontSize === "string" && fontSize === "large") {
-      console.warn("🚨 Invalid fontSize 'large' found in style:", style)
-    }
-  }
-  return originalCreate(styles)
-}
-
-// 🧩 Patch 2: Log invalid fontSize passed directly to <Text>
-const OriginalText = RNText
-function PatchedText(props: RNTextProps) {
-  if (
-    typeof props.style === "object" &&
-    props.style &&
-    !Array.isArray(props.style) &&
-    (props.style as any).fontSize === "large"
-  ) {
-    console.warn("🚨 RNText received fontSize: 'large'", props.style)
-  }
-
-  return <OriginalText {...props} />
-}
-
-// @ts-ignore: Override the default Text component
 
 SplashScreen.preventAutoHideAsync()
 
 function IgniteApp() {
   return <App hideSplashScreen={() => SplashScreen.hideAsync().then(() => true)} />
 }
-
 
 export default IgniteApp
